d3db6626413dd8a1ccd302f23acf10513b9a7010
mudan <myudanov223@gmail.com>
2023-12-07 22:45:33 +0300

    Second commit: create setters and getters ans constructor in Ram

--- oop/Computer.java
+++ oop/Computer.java
@@ -3,4 +3,10 @@ package oop;
 public class Computer {
     private Ram ram;
 
+    public Computer(Ram ram) {
+        this.ram = ram;
+    }
+    public void printStatement(){
+        System.out.println("Computer ram: "+ram.getValue());
+    }
 }
--- oop/Ram.java
+++ oop/Ram.java
@@ -3,4 +3,15 @@ package oop;
 public class Ram {
     private int value;
 
+    public Ram(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
